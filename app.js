814f87ff0408c6e51654aa86d0b0684fc4cb412f
msrdnv <msrdnv@ya.ru>
2023-10-14 17:34:42 +0300

    complete models, routes, limiter, logger, helmet

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { celebrate, Joi, errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { login, createUser } = require('./controllers/users');
+const index = require('./routes/index');
+const auth = require('./middlewares/auth');
+const { limiter } = require('./middlewares/limiter');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+mongoose.connect(DB_URL);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(limiter);
+app.use(requestLogger);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+app.use('/', auth, index);
+app.use(errorLogger);
+app.use(errors());
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ routes/movies.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const urlRegex = require('../utils/regex');
+const { findMovies, createMovie, deleteMovie } = require('../controllers/movies');
+
+router.get('/', findMovies);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.number().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(urlRegex),
+    trailerLink: Joi.string().required().pattern(urlRegex),
+    thumbnail: Joi.string().required().pattern(urlRegex),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+}), createMovie);
+router.delete('/:movieId', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().alphanum().length(24),
+  }),
+}), deleteMovie);
+
+module.exports = router;
